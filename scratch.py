fc7041e6210a1ed67ad3a48f227973fed6c5abb8
Jamie Townsend <jamiehntownsend@gmail.com>
2025-08-09 12:55:09 +0000

    Setup GPT-2 fwd execution in scratch.py

---
+++ scratch.py
@@ -0,0 +1,29 @@
+from jax import random
+import haliax as hax
+
+from levanter.models import gpt2
+from levanter.layers.attention import AttentionMask, AttentionBackend
+
+import scanagram
+
+
+vocab_size = 32
+seq_length = 1024
+Vocab = hax.Axis("vocab", vocab_size)
+Pos = hax.Axis("position", seq_length)
+config = gpt2.Gpt2Config(attn_backend=AttentionBackend.VANILLA)
+k = random.PRNGKey(0)
+k_init, k_inputs, k_eval = random.split(k, 3)
+
+causal_mask = AttentionMask.causal()
+model = gpt2.Gpt2LMHeadModel.init(Vocab, config, key=k_init)
+
+input_ids = random.randint(k_inputs, seq_length, 0, vocab_size)
+
+def gpt2_eval(input_ids):
+    input_ids = hax.named(input_ids, (Pos,))
+    out = model(input_ids, attn_mask=causal_mask, key=k_eval)
+    assert out.axes == (Pos, Vocab)
+    return out.array
+
+out = gpt2_eval(input_ids)
